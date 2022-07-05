88f13cb140c9f8ae03369bc229a2a28abf942f50
Tbrandt-ctrl <thomasbrandt@outlook.fr>
2022-07-05 15:24:19 -0400

    added CV locally

--- locales/components/header/en.ts
+++ locales/components/header/en.ts
@@ -1,7 +1,7 @@
 export default {
   nav_items: [
     { title: "Case Studies", link: "/casestudies" },
-    { title: "Download CV", link: "/" },
+    { title: "Download CV", link: "/files/CV_Klara_Christensen.pdf" },
     { title: "Contact", link: "#contact" },
   ],
 };
--- locales/components/header/fr.ts
+++ locales/components/header/fr.ts
@@ -1,7 +1,7 @@
 export default {
   nav_items: [
     { title: "Études de cas", link: "/fr/casestudies" },
-    { title: "Télécharger le CV", link: "/fr" },
+    { title: "Télécharger le CV", link: "/fr/CV_Klara_Christensen.pdf" },
     { title: "Contact", link: "/fr#contact" },
   ],
 };
